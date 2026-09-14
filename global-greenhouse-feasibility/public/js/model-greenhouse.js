00442d4c288c9653586d47f9f23675c4d692bc76
EdoRaus <edo@dutchgreenhouses.com>
2026-09-14 11:14:43 +0200

    Add greenhouse model

---
+++ global-greenhouse-feasibility/public/js/model-greenhouse.js
@@ -0,0 +1,19 @@
+export const greenhouse = {
+  outsideLength: 99,
+  outsideWidth: 78.4,
+  serviceLength: 16,
+  serviceWidth: 32,
+  siteLength: 120,
+  siteWidth: 100,
+  span: 8,
+  bay: 4.5,
+  height: 6.2,
+  roofAngle: 22.5,
+  roofsPerTruss: 2,
+  ventDepth: 1.4,
+  lighting: 220,
+  topScreen: 30,
+  sideScreen: 90,
+  hangingGutters: 'Yes',
+  fogging: 0.35
+};
